2e643f8a6f84e40660aa1dbf0d8ac1e557389607
Trayshawn <traytwebb14@yahoo.com>
2021-01-22 18:04:01 -0600

    commit tree b9306033238813481d2fdd042863b5d4b8ba38db

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,25 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :comments, reject_if: :reject_comments
+
+  def reject_comments(attributes)
+    attributes['content'].blank?
+  end
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+      end 
+    end 
+  end 
+
+
+
+
+
+
 
 end
